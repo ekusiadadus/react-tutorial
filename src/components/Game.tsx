d30af277bf7876b4264e5608cdf99376f508120f
ekusiadadus <ekusiadadus@gmail.com>
2023-02-03 06:12:03 +0900

    feat: :fire: 過去の着手の表示

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -13,6 +13,15 @@ const Game = () => {
   const status = winner ? `Winner: ${winner}` : `Next player: X`;
   const [xIsNext, setXIsNext] = useState(true);
 
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => {}}>{desc}</button>
+      </li>
+    );
+  });
+
   const handleClick = (i: number) => {
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i] !== i.toString()) return;
@@ -22,15 +31,13 @@ const Game = () => {
   };
 
   return (
-    <div>
-      <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares} onClick={handleClick} />
-        </div>
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={handleClick} />
       </div>
       <div className="game-info">
         <div className="status">{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
