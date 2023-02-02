68034e79bbbb776bfb75acc383b6725a8349a09a
ekusiadadus <ekusiadadus@gmail.com>
2023-02-03 06:05:53 +0900

    feat: :fire: State のリフトアップ、再び

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,90 +1,29 @@
-import React from "react";
 import Square from "./Square";
-import { calculateWinner } from "../helpers/calculateWinner";
 
-const Board = () => {
-  const [squares, setSquare] = React.useState(
-    [...Array(9)].map((_, i) => i.toString())
-  );
-
-  const [xisNext, setXisNext] = React.useState(true);
-
-  const handleClick = (i: number) => {
-    const squaresCopy = squares.slice();
-    if (calculateWinner(squaresCopy) || squaresCopy[i] !== i.toString()) return;
-    squaresCopy[i] = xisNext ? "X" : "O";
-
-    setSquare(squaresCopy);
-    setXisNext(!xisNext);
-  };
-
-  const winner = calculateWinner(squares);
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xisNext ? "X" : "O"}`;
-
+const Board = ({
+  squares,
+  onClick,
+}: {
+  squares: string[];
+  onClick: (i: number) => void;
+}) => {
   return (
     <div>
-      <div className="status">{status}</div>
+      <div className="stauts"></div>
       <div className="board-row">
-        <Square
-          value={squares[0]}
-          onClick={() => {
-            handleClick(0);
-          }}
-        />
-        <Square
-          value={squares[1]}
-          onClick={() => {
-            handleClick(1);
-          }}
-        />
-        <Square
-          value={squares[2]}
-          onClick={() => {
-            handleClick(2);
-          }}
-        />
+        <Square value={squares[0]} onClick={() => onClick(0)} />
+        <Square value={squares[1]} onClick={() => onClick(1)} />
+        <Square value={squares[2]} onClick={() => onClick(2)} />
       </div>
       <div className="board-row">
-        <Square
-          value={squares[3]}
-          onClick={() => {
-            handleClick(3);
-          }}
-        />
-        <Square
-          value={squares[4]}
-          onClick={() => {
-            handleClick(4);
-          }}
-        />
-        <Square
-          value={squares[5]}
-          onClick={() => {
-            handleClick(5);
-          }}
-        />
+        <Square value={squares[3]} onClick={() => onClick(3)} />
+        <Square value={squares[4]} onClick={() => onClick(4)} />
+        <Square value={squares[5]} onClick={() => onClick(5)} />
       </div>
       <div className="board-row">
-        <Square
-          value={squares[6]}
-          onClick={() => {
-            handleClick(6);
-          }}
-        />
-        <Square
-          value={squares[7]}
-          onClick={() => {
-            handleClick(7);
-          }}
-        />
-        <Square
-          value={squares[8]}
-          onClick={() => {
-            handleClick(8);
-          }}
-        />
+        <Square value={squares[6]} onClick={() => onClick(6)} />
+        <Square value={squares[7]} onClick={() => onClick(7)} />
+        <Square value={squares[8]} onClick={() => onClick(8)} />
       </div>
     </div>
   );
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import { useState } from "react";
 import Board from "./Board";
+import { calculateWinner } from "../helpers/calculateWinner";
 
 const Game = () => {
+  const [history, setHistory] = useState([
+    {
+      squares: [...Array(9)].map((_, i) => i.toString()),
+    },
+  ]);
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: X`;
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i: number) => {
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i] !== i.toString()) return;
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(history.concat([{ squares: squares }]));
+    setXIsNext(!xIsNext);
+  };
+
   return (
     <div>
       <div className="game">
-        <Board />
+        <div className="game-board">
+          <Board squares={current.squares} onClick={handleClick} />
+        </div>
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol>{/* TODO */}</ol>
       </div>
     </div>
   );
