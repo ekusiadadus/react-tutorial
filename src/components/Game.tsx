bfeac8246ce7538f02b524d6e4c427a3b51a96c0
ekusiadadus <ekusiadadus@gmail.com>
2023-02-03 06:20:47 +0900

    feat: :fire: タイムトラベルの実装

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,25 +8,36 @@ const Game = () => {
       squares: [...Array(9)].map((_, i) => i.toString()),
     },
   ]);
-  const current = history[history.length - 1];
-  const winner = calculateWinner(current.squares);
-  const status = winner ? `Winner: ${winner}` : `Next player: X`;
   const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+  const status = winner
+    ? "Winner: " + winner
+    : "Next player: " + (xIsNext ? "X" : "O");
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
 
   const moves = history.map((step, move) => {
-    const desc = move ? `Go to move #${move}` : "Go to game start";
+    const desc = move ? "Go to move #" + move : "Go to game start";
     return (
       <li key={move}>
-        <button onClick={() => {}}>{desc}</button>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
       </li>
     );
   });
 
   const handleClick = (i: number) => {
+    const stepHistory = history.slice(0, stepNumber + 1);
+    const current = stepHistory[stepHistory.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i] !== i.toString()) return;
     squares[i] = xIsNext ? "X" : "O";
-    setHistory(history.concat([{ squares: squares }]));
+    setHistory(stepHistory.concat([{ squares }]));
+    setStepNumber(stepHistory.length);
     setXIsNext(!xIsNext);
   };
 
